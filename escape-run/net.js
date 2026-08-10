1542d7022a0adacb46a4f4f840f3f15ef45a1321
ykobashi <yuki.kobayashi.0408@gmail.com>
2026-08-10 22:45:02 +0900

    「協力エスケープラン」を削除
    プレイして単調だったため撤去。トップページのカードと更新履歴からも除去。

--- escape-run/net.js
+++
@@ -1,6 +0,0 @@
-// net.js - 協力エスケープラン: common/net-core.js の薄いラッパー。
-(function () {
-  'use strict';
-  const core = NetCore.create({ roomPrefix: 'escape-run-ykobashi-' });
-  window.EscapeRunNet = { hostRoom: core.hostRoom, joinRoom: core.joinRoom };
-})();
